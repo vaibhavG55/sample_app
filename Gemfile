aeb7e6032476560d52427f51d36ff019a33f4725
Vaibhav gupta <vaibhavgupta556@gmail.com>
2015-01-23 22:53:56 +0530

    Finish sign in

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.18'
-gem 'bootstrap-sass', '2.0.0'
+gem 'bootstrap-sass', '2.2.2'
 gem 'bcrypt-ruby', '3.0.1'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -9,6 +9,8 @@ gem 'bcrypt-ruby', '3.0.1'
 group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.9.0'
+  gem 'cucumber-rails', '1.2.1', require: false
+  gem 'database_cleaner', '0.7.0'
 end
 
 # Gems used only for assets and not required
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -48,6 +48,13 @@ describe "UserPages" do
            let(:user) { User.find_by_email("vaibhav.gupta.cse11@iitbhu.ac.in") }
            it { should have_selector('title', text: user.name) }
            it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+           it {should have_link('Sign out')}
+           
+           describe "followed by signout" do
+              before{click_link 'Sign out'}
+              it{should have_link 'Sign in'}
+           end
+           
         end
      end
      
