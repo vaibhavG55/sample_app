c11a2edc46a881bc93da75bf0f3490d77eea9654
Vaibhav gupta <vaibhavgupta556@gmail.com>
2015-03-23 15:42:43 +0530

    Updating users and Authorization done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       #Sign in successful
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       #Sing in Failed
       flash.now[:error] = 'Invalid Password or Email' 
